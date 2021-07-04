462347be6de0a44cdb8304588266434e8dcc9028
Atilio <atiliogoulart@gmail.com>
2021-07-04 06:58:41 -0300

    1.6

--- app/products/urls.py
+++ app/products/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
 
     path('', views.AllProducts.as_view()),
     path('categorys/', views.AllCategorys.as_view()),
+    path('create/', views.AddProduct.as_view()),
+    path('products/<int:id>/', views.ProductById.as_view()),
+
 
 ]
--- app/products/views.py
+++ app/products/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -19,3 +20,42 @@ class AllCategorys(APIView):
 
         products_categorys = [category.category for category in ProductModel.objects.all()]
         return Response(products_categorys)
+
+class AddProduct(APIView):
+
+
+    def post(self, request):
+
+
+        data = {
+
+            'name_product': request.data.get('name_product'),
+            'plataform': request.data.get('plataform'),
+            'price': request.data.get('price'),
+            'restaurant': request.data.get('restaurant'),
+            'category': request.data.get('category'),
+
+        }
+
+        products = ProductSerializer(data=data)
+
+        if products.is_valid():
+            products.save()
+            return Response(products.data)
+        else:
+            return Response(products.errors)
+
+
+class ProductById(APIView):
+
+    def get_product(self, id):
+        try:
+            return ProductModel.objects.get(id=id)
+        except ProductModel.DoesNotExist:
+            return Http404
+
+    def get(self, request, id,format=None):
+
+        product = self.get_product(id=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
